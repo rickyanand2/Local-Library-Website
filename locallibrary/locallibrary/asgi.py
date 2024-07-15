11bd74820ce726782b83abcf637ef5af2a4bd492
Ricky Anand <rickyanand2@gmail.com>
2024-07-15 11:17:18 +1000

    Locallibrary project and catalog app created

--- locallibrary/locallibrary/asgi.py
+++ locallibrary/locallibrary/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for mytestsite project.
+ASGI config for locallibrary project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytestsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
 
 application = get_asgi_application()
